44925a1c8c6e336914aa796b26013f8f17610032
Nikola Pacaric <nikola.pacaric.25@singimail.rs>
2026-07-19 20:04:14 +0200

    Napravljen i dodat spring boot. Definisan i raspakovan preko Spring Initializr, i dodata konekcija sa MySQL lokalnom bazom

---
+++ backend/src/main/java/com/uniproject/library/LibraryApplication.java
@@ -0,0 +1,14 @@
+package com.uniproject.library;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LibraryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LibraryApplication.class, args);
+
+	}
+
+}
